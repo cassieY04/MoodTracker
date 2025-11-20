0c265c155160e388ce4d1bb0a38005c6dde319c9
Jia Qi <lujiaqi2006@gmail.com>
2025-11-20 08:51:50 +0800

    made some changes to users.py to link to database for check user existence, add new user and get(read/login) user

--- users.py
+++ users.py
@@ -1,17 +1,44 @@
-users = {}
+from Databases.userdb import get_db
+import sqlite3
 
 class UserManager:
     @staticmethod
     def user_exists(username):
-        return username in users
+        db = get_db()
+        try:
+            cursor = db.execute('SELECT 1 FROM users WHERE username = ?', (username))
+            return cursor.fetchone() is not None
+        finally:
+            db.close()
     
     @staticmethod
     def add_user(username, user_data):
-        users[username] = user_data
+        db = get_db()
+        try:
+            db.execute(
+                'INSERT INTO users (username, phone, email, password) VALUES (?, ?, ?, ?)',
+                (username, user_data['phone'], user_data['email'], user_data['password'])
+            )
+            db.commit()
+        except sqlite3.IntegrityError:
+            db.rollback()
+            raise ValueError('Username or Email already exist.')
+        finally:
+            db.close()
+        
     
     @staticmethod
     def get_user(username):
-        return users.get(username)
+        db = get_db()
+        try:
+            cursor = db.execute('SELECT username, password FROM users WHERE username = ?')
+            user_row = cursor.fetchone()
+            if user_row:
+                return dict(user_row)
+            return None
+        finally:
+            db.close()
+
     
     @staticmethod
     def update_user(username, user_data):
