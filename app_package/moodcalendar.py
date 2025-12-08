d779b5110fbf07f7d2e1d44a216036274e75257b
santania24 <santanialing@gmail.com>
2025-12-08 22:35:35 +0800

    getting started

--- app_package/moodcalendar.py
+++ app_package/moodcalendar.py
@@ -4,4 +4,15 @@ mood_calendar_bp = Blueprint('mood_calendar', __name__)
 
 @mood_calendar_bp.route("/mood_calendar")
 def mood_calendar():
-    return render_template("mood_calendar.html")
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    month_name = now.strftime("%B")
+    
+    cal_data = calendar.monthcalendar(year, month)
+    return render_template("mood_calendar.html",
+                            year=year,
+                            month_name=month_name,
+                            calendar=cal_data,
+                            mood_data=mock_mood_data
+    )
